1b5f3cea64e7ad6847e7ae2b760a31eec844fc96
HannanBhuiyan <hannan.diuh@gmail.com>
2022-06-13 19:54:08 +0600

    53 problem solve

--- Practice_01/Problem_01.cs
+++ Practice_01/Problem_01.cs
@@ -246,7 +246,7 @@ namespace Practice_01
             } 
 
 
-            // problem 47:
+            //  ================== problem 47:
             int[] arr =  {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
             int sum = 0;
             for(int i = 0; i< arr.Length; i++)
@@ -256,7 +256,7 @@ namespace Practice_01
             Console.WriteLine(sum);
 
 
-             // problem 48:
+             //  ================== problem 48:
              int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 55 };
 
             int firstindex = nums[0];
@@ -272,7 +272,7 @@ namespace Practice_01
             }
 
 
-             // problem 49:
+             //  ================== problem 49:
             int[] arr1 = new int[] { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
             int[] arr2 = new int[] { 10, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5 };
 
@@ -289,25 +289,152 @@ namespace Practice_01
             }
              
 
-            // problem 50:
-
+            //  ================== problem 50:
             int[] arr = { 1, 2, 8, 10 };
-
             int temp = arr[0];
-
             for(int i =0; i < arr.Length - 1; i++)
             {
                 arr[i] = arr[i + 1];
             }
-
             arr[arr.Length - 1] = temp;
-
             Console.WriteLine($"[{string.Join(",", arr)}]");
+
+
+             //  ================== problem 51:
+
+            // way one
+            //int[] array = { 1, 2, 5, 55, 7, 8 };
+            //int maxValue = array.Max();
+
+            //int maxIndex = array.ToList().IndexOf(maxValue);
+            //Console.WriteLine(array[maxIndex]);
+
+
+            // way three
+
+            //int[] array = { 1, 2, 5, 55, 7, 8, 100 };
+            //int index = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] > index)
+            //    {
+            //        index  = array[i];
+            //    }
+            //}
+            //Console.WriteLine(index);
+
+
+            //int[] array = { 1, 2, 5, 55, 500, 7, 8, 100 };
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //     for(int j = 0; j < array.Length - 1; j++)
+            //    {
+            //        if(array[j] > array[j + 1])
+            //        {
+            //            int temp = 0;
+            //            temp = array[j];
+            //            array[j] = array[j + 1];
+            //            array[j + 1] = temp;
+                         
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(array[array.Length - 1]);
+
+
+
+
+             // ================== problem 52:
+
+            int[] arr1 = { 1, 2, 11 };
+            int[] arr2 = { 6, 0, 5 };
+            int[] arr3 = { 3, 4, 5 };
+
+           
+            int mid = arr1[arr1.Length / 2];
+            Console.WriteLine(mid);
+
+
+            // way one
+            //int arr1Middle = arr1[arr1.Length - 2];
+            //int arr2Middle = arr2[arr2.Length - 2];
+            //int arr3Middle = arr3[arr3.Length - 2];
+
+            //int arr1Middle = arr1[1];
+            //int arr2Middle = arr2[1];
+            //int arr3Middle = arr3[1];
+
+            //int[] new_array = { arr1Middle, arr2Middle, arr3Middle };
+            //Console.WriteLine($"[{string.Join(",", new_array)}]");
+            //Console.WriteLine($"[{ String.Join(", ", arr1Middle, arr2Middle , arr3Middle)}]");
+
+
+
+            int[] ar = { 1, 5, 7, 9, 11, 13 };
+            Console.WriteLine($"[{ string.Join("," , ar[ar.Length / 2 - 1], ar[ar.Length / 2])}]");
+
+            //foreach(var i in new_arr)
+            //{
+            //    Console.Write(i.ToString()+" ");
+            //}
+
+
+
+              // ================= array copy extra problem ===============
+
+            int[] ar = { 1, 5, 7, 9, 11, 13 };
+
+            //var new_arr = new int[6];
+
+            //ar.CopyTo(new_arr, 0);
+
+            //foreach(var i in new_arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            var new_arr = new int[6];
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                new_arr[i] = ar[i];
+
+            }
+
+            foreach (var i in new_arr)
+            {
+                Console.WriteLine(i);
+            }
+             
+            // ================= array copy extra problem ===============
+
+
+
+            // problem 53:
+
+            int[] arr = { 2, 4, 10, 8, 6 };
+
+            bool isOdd = false;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] % 2 != 0)
+                {
+                    isOdd = true;
+                }
+            }
+            Console.WriteLine(isOdd);
+
 
 
              */
 
-             
+
+
+
+
+
+
 
 
         }
